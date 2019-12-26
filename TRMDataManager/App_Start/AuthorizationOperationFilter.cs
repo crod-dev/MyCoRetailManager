795a599154d5073b423dadebd5e36086b1fa634e
CRod Dev <crod.dev@gmail.com>
2019-12-26 09:59:07 -0700

    Initial check-in from lessons 1-7

---
+++ TRMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,37 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace TRMDataManager.App_Start
+{
+
+    /// <summary>
+    /// 04 This opertion filter will add a parameter to every operation.
+    /// Add an access token everytime to be able to authenticate
+    /// </summary>
+    
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            // 04 check to see if there are no paramters
+            if(operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+            // 04 create and add the parameter
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,61 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        // 07 Caliburn Micro (nuget package previously installed for MVVC) contains a SimpleContainer class to help
+        // with dependency injection; Instantiate SimpleContainer object to handle dependency injection 
+        private SimpleContainer _container = new SimpleContainer();
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        // 07 Where the actual instantiation happens; tells the container what to connect to what
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            // 07 Some Caliburn Micro features, WindowManager handles bringing windows in and out
+            // EventAggregator ties application together, passes event messaging throughout app. One section can raise an event
+            // and another can listen and handle it; ties interface to implementation
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            // 07 Use reflection to tie ViewModels to Views, this code only runs once on startup to performance impact is minimal
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+        }
+        // 05 Specifies ShellViewModel to launch on startup as base view; VewModel launches the View
+        // 05 Remove start up from App.xaml file
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+        // 07 Pass in a type and name and will return a container
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        // 07 Retern all instances of objects of certain type using the container
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        // 07 This is where the container builds the objects
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
+    }
+}
