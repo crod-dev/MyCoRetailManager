ed8eb813cbc349f07e0d447941c6bee2c21a9064
CRod Dev <crod.dev@gmail.com>
2019-12-30 09:04:52 -0700

    Register DB tables Tables for the basic register system

---
+++ TRMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Product_Id] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT GETUTCDATE()
+)
---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Sale_Id] INT NOT NULL, 
+    [Product_Id] INT NOT NULL, 
+    [Quantity] INT NOT NULL DEFAULT 1, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [Tax] MONEY NOT NULL DEFAULT 0
+)
